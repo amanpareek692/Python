30a2b46be91369e8df706c222dd3283e7a47c697
amanpareek692 <coderaman123@gmail.com>
2026-06-28 13:16:19 +0530

    Added Day_01 folder

--- Day_01/1.py
+++ Day_01/1.py
@@ -1,2 +1,7 @@
-a=[1,2,3]
-print(a)
+"""Take the user's name, age and height as input.
+Print a sentence using an f-string like this: 
+"""
+Name=input('Enter the name of the Person:')
+Age=int(input('Enter the Age of the person:'))
+Height=input("Enter the Height of the Person")
+print(f"Hi {Name}! After Ten years, Your age will be {Age+10} Years old and Your height is {Height} feet.  ")
---
+++ Day_01/2.py
@@ -0,0 +1,18 @@
+"""Take two number and print:
+Addition
+Subtraction
+Multiplication
+Division
+Floor Division
+Modulus
+Exponent"""
+Number_1=int(input("Enter the number 1:"))
+Number_2=int(input("Enter the number 2:"))
+print(Number_1+Number_2)
+print(Number_1-Number_2)
+print(Number_1*Number_2)
+d=Number_1/Number_2
+print(round(d,2))
+print(Number_1//Number_2)
+print(Number_1%Number_2)
+print(Number_1**Number_2)
