9645f3a305fc0dd63656c3673b423cca6552af76
amanpareek692 <coderaman123@gmail.com>
2026-07-01 13:56:58 +0530

    Added  Day_02 folder

--- Day_02/3.py
+++ Day_02/3.py
@@ -1,3 +1,13 @@
+"""Create:
+A list of 6 Fruits(with duplicate value)
+convert into a set
+convert the set into tuple
+Then-
+if the number of unique is greater than 4,print Good variety
+otherwise print less variety
+Also print all three data structure."""
+
+
 Fruits = ["Banana","Apple","Orange","Mango","gold","jamun"]
 set_1 = set(Fruits)
 Tuple_1 = tuple(set_1)
